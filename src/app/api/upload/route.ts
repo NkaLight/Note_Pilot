1f8316cdf78712a71bc848ee5f1494041c0d5b58
hubda444 <hubda444@student.otago.ac.nz>
2025-08-28 08:46:26 +1200

    reference code for amazon s3 file storage, unimplemented

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,8 +1,47 @@
 import { NextResponse } from "next/server";
 import { pdf_processing } from "@/lib/pdf_processing";
 import { summarizeWithOllama } from "@/lib/ollama";
+//import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 export const runtime = "nodejs";
+
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started-nodejs.html
+/*
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+// uploads a file to a bucket
+const s3Key = `${user_id}/${fileName}`;
+  await s3Client.send(
+    new PutObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME!,
+      Key: s3Key,
+      Body: fileBuffer,
+    })
+);
+
+// create a record in the database for the paper and upload, not sure how to handle this yet
+const paper = await prisma.paper.create({
+    data: {
+      user_id: userId,
+      filename: fileName,
+    },
+});
+
+await prisma.upload.create({
+    data: {
+      paper_id: paper.paper_id,
+      filename: fileName,
+      storage_path: s3Key,
+    },
+});
+*/
+
 
 export async function POST(req: Request) {
   try {
