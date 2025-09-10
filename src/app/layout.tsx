14a7d8fda8aa3003ba8b149110d34f993e032d2c
HaroldPonce22 <ponha312@student.otago.ac.nz>
2025-09-11 05:54:05 +1200

    chore(layout): wrap with ThemeProviders/SessionProvider and suppress hydration warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,31 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Nav from "@/components/Nav";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { SessionProvider } from "@/context/SessionContext";
+import ThemeInit from "@/components/Account/themeInit";
+import { ThemeProviders } from "@/components/Account/themeProvider";
+
 
 export const metadata: Metadata = {
   title: "Note Pilot",
   description: "",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
       <body>
-        <SessionProvider>
-          <Nav/>
-          <main>
-            {children}
-          </main>
-          <Footer/>
-        </SessionProvider>
+      <ThemeProviders attribute="class" defaultTheme="light" enableSystem={false}>
+          <SessionProvider>
+            <ThemeInit /> {/* syncs theme to user preference if available */}
+            <Nav />
+            <main>{children}</main>
+            <Footer />
+          </SessionProvider>
+        </ThemeProviders>
       </body>
     </html>
   );
