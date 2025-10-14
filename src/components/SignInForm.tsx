e3e734290234bda387a1b35ebd2fcbf31f34198c
sytne506 <sytne506@student.otago.ac.nz>
2025-10-14 16:48:35 +1300

    Fix import order in SignInForm component

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useState } from "react";
 import { useSession } from "@/context/SessionContext";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 // Define the type for the props
 interface SignInFormProps {
   closeForm: () => void;
