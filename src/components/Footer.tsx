c7e45cd3be9cf5f11a5f6fa6e1ebc9f30e28647d
tucan747 <tucan747@student.otago.ac.nz>
2025-10-16 11:32:19 +1300

    footer carousel

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,25 @@
 export default function Footer(){
     return(
         <div className="footer-section">
-            <p>&copy; Note Pilot</p>
+            <div className="carousel-track">
+            <p>&copy;NotePilot</p>
+            <p>est.2025</p>
+            <p>Nkanyiso Ndlovu</p>
+            <p>&copy; NotePilot</p>
+            <p>est.2025</p>
+            <p>Daniel Hubers</p>
+            <p>&copy; NotePilot</p>
+            <p>est.2025</p>
+            <p>
+            Harold Ponce Saldivar</p>
+            <p>&copy; NotePilot</p>
+            <p>est.2025</p>
+            <p>Angus 
+            Tucker</p>
+            <p>&copy; NotePilot</p>
+            <p>est.2025</p>
+            <p>Newton Sythong</p>
+            </div>
         </div>
     )
 }
