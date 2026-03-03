c3f97cc04cf6e0b4a5d80ca76fdb77c914eea836
Nkanyiso <ndlnk218@student.otago.ac.nz>
2026-03-04 00:43:04 +1300

    fixed typo

--- src/app/api/refresh_token/route.ts
+++ src/app/api/refresh_token/route.ts
@@ -1,8 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { z, ZodError } from "zod"; 
+import { z, } from "zod"; 
 import { refreshLogic,} from "@/lib/services/refresh_token";
-import { printCurrentSessionCache } from "@/lib/session";
 
 
 /** Use ZOD to validate structure */
@@ -14,11 +13,8 @@ export async function POST(req:NextRequest){
     try{
         const refresh_token = (await cookies()).get("refresh_token")?.value;
         validate_session_schema.parse({refresh_token});
-        const data = await refreshLogic(refresh_token);
-        if(!data) return NextResponse.json({user:null, status:401});
-        const user = data.application_user.application_user;
-        console.error(user);
-        printCurrentSessionCache();
+        const user = await refreshLogic(refresh_token);
+        if(!user) return NextResponse.json({user:null, status:401});
         return NextResponse.json({ user: { id: user.user_id, email: user.email, username : user.username } });
     }catch(error){
         console.error(error);
