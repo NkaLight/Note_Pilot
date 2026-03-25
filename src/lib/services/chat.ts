88904d6d11bd4d97636999fec6481c5544947a7e
Nkanyiso <nkanyisoowethu@gmail.com>
2026-03-25 23:46:45 +1300

    feat: after stream complete chatmessages persisted DB

--- src/lib/services/chat.ts
+++ src/lib/services/chat.ts
@@ -1,4 +1,4 @@
-import { queryLLMChat, queryLLMChatStream } from "../utils/ai-gateway";
+import { queryLLMChat, queryLLMChatStream, StreamChunk } from "../utils/ai-gateway";
 import { getChatMessages, saveNewMessages } from "../db_access/chat_message";
 import { ChatMessage } from "../utils/ai-gateway";
 import { ServiceError, ServiceType } from "../error";
@@ -39,5 +39,40 @@ export async function streamChat(uploadId:number ,userId: number, content: strin
     ...priorMessages.map(m => ({ role: m.role as "user" | "assistant", content: m.content })),
     { role: "user", content },
     ];
-    return queryLLMChatStream(systemPrompt, history, {type:ServiceType.CHAT_AI});
+
+    let LLMText = "";
+    const stream = await queryLLMChatStream(systemPrompt, history, {type:ServiceType.CHAT_AI});
+    return new ReadableStream({
+        async start(controller){
+            const reader = stream.getReader();
+            const textCode = new TextDecoder();
+            try{
+                while(true){
+                    const {done, value} = await reader.read();
+                    if(done){
+                        break;
+                    }
+                    const raw = textCode.decode(value, {stream:true});
+                    const lines = raw.split("\n").filter((l)=> l.startsWith("data: "));
+                    for(const line of lines){
+                        try{
+                            const parsed:StreamChunk = JSON.parse(line.slice(6));
+                            if(parsed.type === "delta")LLMText += parsed.text;
+                            if(parsed.type === "done"){
+                                await saveNewMessages(uploadId, userId, content, LLMText); 
+                            }
+                        }catch{
+                            //ignore
+                        }
+                    } 
+                    controller.enqueue(value);
+                }
+                controller.close();
+            }catch{
+                controller.error("Stream interupted");
+            }
+        },cancel(){
+            stream.cancel();
+        }
+    });
 }
